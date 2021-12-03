f1c46321c247e2cec4df81d8744cf5702bb4edab
da-Igtm <85411715+da-Igtm@users.noreply.github.com>
2021-12-03 16:40:50 +0530

    Add files via upload

---
+++ listen_to_songs.js
@@ -0,0 +1,12 @@
+
+function playSong1(){
+document.getElementById("audio1").play();
+document.getElementById("audio2").pause();
+}
+function playSong2(){
+document.getElementById("audio2").play();
+document.getElementById("audio1").pause();  
+}
+function playSong3(){
+document.getElementById("audio2").play();   
+} 
